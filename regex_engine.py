32e539ea32d6ab61d2d6e16a6f78b0ade8274cbc
Jeffresh <jeff.uca@gmail.com>
2020-09-18 10:54:00 +0200

    :sparkles: add stage 2/6 working with strings of different length

--- regex_engine.py
+++ regex_engine.py
@@ -1,7 +1,24 @@
-def my_regex(regex_input):
-    pattern, input_string = regex_input.split('|')
+def my_regex(pattern, input_string):
     return pattern == '' or (pattern == '.' and bool(input_string)) or pattern == input_string
 
 
+def regex_engine(raw_input):
+    split_pattern = '|'
+    pattern, input_string = raw_input.split(split_pattern)
+    return engine(pattern, input_string)
+
+
+def engine(pattern, input_string):
+    if not pattern:
+        return True
+    elif not input_string:
+        return False
+    else:
+        if my_regex(pattern[0], input_string[0]):
+            return engine(pattern[1:], input_string[1:])
+        else:
+            return False
+
+
 if __name__ == '__main__':
-    print(my_regex(input()))
+    print(regex_engine(input()))
