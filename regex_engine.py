43e343b930fb6a240de77ec82e08a67c0023f045
Jeffresh <jeff.uca@gmail.com>
2020-09-22 14:12:52 +0200

    :recycle: change name of my_regex to process_character to improve readability

--- regex_engine.py
+++ regex_engine.py
@@ -10,7 +10,7 @@ def equal_character(pattern, character):
     return pattern == character
 
 
-def my_regex(pattern_character, character):
+def process_character(pattern_character, character):
     return dot_wildcard(pattern_character, character) \
            or equal_character(pattern_character, character) \
            or special_rules(pattern_character, character)
@@ -18,4 +18,4 @@ def my_regex(pattern_character, character):
 
 if __name__ == '__main__':
     character_1, character_2 = input().split("|")
-    print(my_regex(character_1, character_2))
+    print(process_character(character_1, character_2))
